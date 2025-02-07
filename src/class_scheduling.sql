be13c077c6b6ece6cdf5f9947cfa9404199f3623
Wilbur Summerson <wilbur.summerson@ada.ac.uk>
2025-02-07 17:11:20 +0000

    halfway through queries all working up to 4.2

--- src/class_scheduling.sql
+++ src/class_scheduling.sql
@@ -1,16 +1,64 @@
 -- Initial SQLite setup
-.open fittrackpro.db
+.open fittrackpro.sqlite
 .mode column
 
 -- Enable foreign key support
+PRAGMA foreign_key = ON;
 
 -- Class Scheduling Queries
 
 -- 1. List all classes with their instructors
 -- TODO: Write a query to list all classes with their instructors
 
+SELECT classes.class_id, classes.name AS class_name, (staff.first_name || ' ' || staff.last_name) as instructor_name
+FROM classes
+JOIN class_schedule on classes.class_id = class_schedule.class_id
+JOIN staff on class_schedule.staff_id = staff.staff_id;
+
 -- 2. Find available classes for a specific date
 -- TODO: Write a query to find available classes for a specific date
+
+-- SELECT classes.capacity
+--     FROM classes
+--     JOIN class_schedule ON classes.class_id = class_schedule.class_id
+--     WHERE (strftime('%Y-%m-%d', class_schedule.start_time) = '2025-02-01')
+--     GROUP BY classes.class_id;
+
+-- SELECT COUNT(*) 
+--     FROM class_attendance 
+--     JOIN class_schedule ON class_attendance.schedule_id = class_schedule.schedule_id
+--     WHERE (strftime('%Y-%m-%d', class_schedule.start_time) = '2025-02-01') 
+--         AND (class_attendance.attendance_status = 'Registered')
+--     GROUP BY class_schedule.class_id;
+
+-- SELECT classes.capacity
+-- FROM classes
+-- JOIN class_schedule ON classes.class_id = class_schedule.class_id
+-- WHERE (strftime('%Y-%m-%d', class_schedule.start_time) = '2025-02-01')
+-- GROUP BY classes.class_id;
+    
+-- SELECT COUNT(*) 
+-- FROM class_attendance 
+-- JOIN class_schedule ON class_attendance.schedule_id = class_schedule.schedule_id
+-- WHERE (strftime('%Y-%m-%d', class_schedule.start_time) = '2025-02-01') 
+--     AND (class_attendance.attendance_status = 'Registered')
+-- GROUP BY class_schedule.class_id
+
+SELECT classes.class_id, classes.name, class_schedule.start_time, class_schedule.end_time,
+    ((SELECT classes.capacity
+    FROM classes
+    JOIN class_schedule ON classes.class_id = class_schedule.class_id
+    WHERE (strftime('%Y-%m-%d', class_schedule.start_time) = '2025-02-01')
+    GROUP BY classes.class_id) -
+    (SELECT COUNT(*) 
+    FROM class_attendance 
+    JOIN class_schedule ON class_attendance.schedule_id = class_schedule.schedule_id
+    WHERE (strftime('%Y-%m-%d', class_schedule.start_time) = '2025-02-01') 
+        AND (class_attendance.attendance_status = 'Registered')
+    GROUP BY class_schedule.class_id)) AS available_spots
+FROM classes
+JOIN class_schedule ON classes.class_id = class_schedule.class_id
+WHERE (strftime('%Y-%m-%d', class_schedule.start_time) = '2025-02-01')
 
 -- 3. Register a member for a class
 -- TODO: Write a query to register a member for a class
--- src/user_management.sql
+++ src/user_management.sql
@@ -1,25 +1,65 @@
 -- Initial SQLite setup
-.open fittrackpro.db
+.open fittrackpro.sqlite
 .mode column
 
 -- Enable foreign key support
+PRAGMA foreign_key = ON;
 
 -- User Management Queries
 
 -- 1. Retrieve all members
 -- TODO: Write a query to retrieve all members
 
+SELECT *
+FROM members;
+
 -- 2. Update a member's contact information
 -- TODO: Write a query to update a member's contact information
+
+UPDATE members
+SET phone_number = '555-9876',
+    email = 'emily.jones.updated@email.com'
+WHERE member_id = 5;
+
+SELECT *
+FROM members
+WHERE member_id = 5;
 
 -- 3. Count total number of members
 -- TODO: Write a query to count the total number of members
 
+SELECT COUNT(*) AS number_members
+FROM members;
+
 -- 4. Find member with the most class registrations
 -- TODO: Write a query to find the member with the most class registrations
+
+WITH RegistrationCounts AS (
+    SELECT m.member_id, m.first_name, m.last_name, COUNT(ca.schedule_id) AS registration_count
+    FROM members m
+    JOIN class_attendance ca ON m.member_id = ca.member_id
+    GROUP BY m.member_id
+)
+SELECT member_id, first_name, last_name, registration_count
+FROM RegistrationCounts
+WHERE registration_count = (SELECT MAX(registration_count) FROM RegistrationCounts);
 
 -- 5. Find member with the least class registrations
 -- TODO: Write a query to find the member with the least class registrations
 
+WITH RegistrationCounts AS (
+    SELECT m.member_id, m.first_name, m.last_name, COUNT(ca.schedule_id) AS registration_count
+    FROM members m
+    JOIN class_attendance ca ON m.member_id = ca.member_id
+    GROUP BY m.member_id
+)
+SELECT member_id, first_name, last_name, registration_count
+FROM RegistrationCounts
+WHERE registration_count = (SELECT MIN(registration_count) FROM RegistrationCounts);
+
 -- 6. Calculate the percentage of members who have attended at least one class
 -- TODO: Write a query to calculate the percentage of members who have attended at least one class
+
+SELECT (COUNT(DISTINCT ca.member_id) * 100 / (SELECT COUNT(*) FROM members)) AS percentage_of_members
+FROM class_attendance ca
+WHERE attendance_status = 'Attended';
